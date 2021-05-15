6aafa4907283206e1094d51c4c8594f0e468f85b
fhaussener <fabio.haussener@gmail.com>
2021-05-15 15:03:32 +0200

    Add mailchimp field

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
 import { NavigationBar } from "../components/NavigationBar";
 
-function Home() {
+function Home({ mailchimpLink }) {
   return (
     <div>
       <Head>
@@ -11,13 +12,17 @@ function Home() {
       <NavigationBar />
       <main>
         <h1>Home landingpage</h1>
-
-        <p>Some stuff about something</p>
+        <h3>Subscribe for product updates</h3>
+        <MailchimpSubscribe url={mailchimpLink} />
       </main>
 
       <footer>Some footer</footer>
     </div>
   );
 }
+
+Home.getInitialProps = () => {
+  return { mailchimpLink: process.env.NEXT_MAILCHIMP_API_URL };
+};
 
 export default Home;
