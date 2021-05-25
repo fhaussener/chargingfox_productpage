84f4240557d6a74d368c16637fe1796085cdc22f
fhaussener <fabio.haussener@gmail.com>
2021-05-25 10:57:13 +0200

    Add map

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,5 +1,24 @@
+import * as React from "react";
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
 import styles from "./index.module.scss";
 
-export const MapLocations = () => {
-  return <div className={styles.MapContainer}></div>;
+export const MapLocations = ({ mapboxToken }) => {
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: 400,
+    latitude: 47.37218571132273,
+    longitude: 8.539590813947916,
+    zoom: 12,
+  });
+  return (
+    <div className={styles.MapContainer}>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={mapboxToken}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      />
+    </div>
+  );
 };
